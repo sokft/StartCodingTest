7df6d9c767c2a80c211fd331903a5efe12f1ec43
Jikang Han <hanjk0816@naver.com>
2025-07-17 09:34:15 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -1,4 +1,5 @@
 
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -40,35 +41,47 @@ public class Main {
     }
 
     private static int jMoney(int[] arr, int money) {
-        int stock =0;
-        for (int i =0; i < arr.length; i++) {
-            if(money >= arr[i]) {
+        int stock = 0;
+        //만약 100, 10 -> 10주, 15, (20, 20, 6) -> 0, 0, 1주
+        /*
+        그니까 if(money > arr[i]) -> stock = money /arr[i], money= money-arr[i]*stock
+        money가  money를 주가로 나누면 stock,
+         */
+        for(int i = 0; i < arr.length; i++) {
+            if (money >= arr[i]) {
                 int buy = money/arr[i];
                 stock += buy;
-                money -= buy*arr[i];
+                money = money - arr[i]*buy;
+            } else {
+                continue;
             }
-
         }
-
-
         return money + stock* arr[13];
     }
 
     private static int sMoney(int[] arr, int money) {
-        int stock =0;
+        int stock = 0;
+        /*
+        10, 20, 23, 34, 55, 30, 22, 19
+        3일 하락 -> 똑같이 buy
+        3일 상승 -> sell 전부 -> money += stock*주가
+         */
 
-        for (int i = 3; i < arr.length; i++) {
-            if (arr[i - 3] > arr[i - 2] && arr[i - 2] > arr[i - 1]) {
-                if (money >= arr[i]) {
-                    int buy = money / arr[i];
-                    stock += buy;
-                    money -= buy * arr[i];
-                }
-            } else if (arr[i - 3] < arr[i - 2] && arr[i - 2] < arr[i - 1]) {
-                money += stock * arr[i];
+        for (int i =3; i < arr.length; i++) {
+            //하락할때
+            if(arr[i-3] > arr[i-2] && arr[i-2] > arr[i-1]) {
+                int buy = money/arr[i];
+                stock += buy;
+                money -= buy*arr[i];
+            }
+            //상승할때
+            else if(arr[i-3] < arr[i-2] && arr[i-2] < arr[i-1]) {
+                money += stock*arr[i];
                 stock = 0;
             }
+
         }
+
 
         return money + stock * arr[13];
     }
